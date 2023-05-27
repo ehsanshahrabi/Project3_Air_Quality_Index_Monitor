7142f1a17deee6226a3bebed9c3a92008b15e3c6
MirianRuanova <mruanova.us@gmail.com>
2023-05-26 23:51:33 -0700

    Getting the first map

--- MongoDB_Flask.py
+++ MongoDB_Flask.py
@@ -1,8 +1,10 @@
 #We want to connect our MongoDB instance to the Flask application. 
 from flask_pymongo import PyMongo
 import flask
+from flask import Flask, render_template, jsonify
 import json
 from bson import ObjectId
+import re
 
     
 #We create our Flask app object:
@@ -17,15 +19,30 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/air_quality_status"
 mongodb_client = PyMongo(app)
 db = mongodb_client.db
 
-#Retrieving Data From the Database
 @app.route("/")
 def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+#Retrieving Data From the Database
+@app.route("/data")
+def data():
     fields= {'data'}
     air_quality_status = db.air_quality_status.find(projection = fields )
 
     air = [ airq['data'] for airq in air_quality_status]
     print(len(air))
     return  flask.jsonify(air)
+@app.route('/cityname')
+def cityname():
+    fields = {'data.city.name': 1}
+    citynames = db.air_quality_status.find({},fields)
+    thecityname = [re.sub(r'(?<!\w\\)\s*\([^)]*\)', '', n['data']['city']['name']) for n in citynames]
+    print(len(thecityname))
+    return  flask.jsonify(thecityname)
 
 if __name__ == '__main__':
     app.run(debug=True)
